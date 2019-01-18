422267c27a1f5cacf1e36b8db1b973a405b4763d
Victor CUAU <victor.cuau@hotmail.fr>
2019-01-18 11:55:53 +0100

    babyStep : Implémentation de la classe Server

--- src/babyStep/Server.java
+++ src/babyStep/Server.java
@@ -1,5 +1,69 @@
 package babyStep;
 
-public class Server {
+import java.io.*;
+import java.net.*;
 
+public class Server {
+	
+	int port ;
+	ServerSocket server ;
+	
+	Server(int port) {
+		// Création d'un socket sur le serveur
+		this.port = port ;
+		try {
+			server = new ServerSocket(port) ;
+			System.out.println("Serveur started");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("IOException in Server()");
+		}
+	}
+	
+	public void main() {
+		while (true) {
+			// Le serveur est en attente de connexion
+			
+			Socket client;
+			try {
+				client = server.accept();
+				
+				// Un client est connecté
+				
+				System.out.println("Client" + client.getInetAddress() + "connected.");
+				
+				InputStream is = client.getInputStream();
+				DataInputStream dis = new DataInputStream(is);
+				OutputStream os = client.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(os);
+				
+				// Réception du nom du client
+				int length_in = dis.readInt();
+				byte[] b_in = new byte[length_in];
+				int nread = 0;
+				int num = 0;
+				while (nread < length_in) {
+					num = dis.read(b_in, nread, length_in-nread);
+					if (num == -1) {
+						return;
+					}
+				}
+				String client_name = new String(b_in);
+				System.out.println("Client said: " + client_name);
+				
+				// Emission du message "Hello <client_name>"
+				String message = "Hello" + client_name ;
+				byte[] b_out = message.getBytes();
+				dos.writeInt(b_out.length);
+				os.write(b_out);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("IOException in main()");
+			}
+			
+			
+		}
+	}
+	
 }
