b3c9f146ddd0238c8aed01ead9e9fca805f751fc
Victor CUAU <victor.cuau@hotmail.fr>
2019-01-19 14:40:38 +0100

    basicFileServer : Création du nouveau package

---
+++ src/basicFileServer/Client.java
@@ -0,0 +1,65 @@
+package basicFileServer;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+	public static void main(String args[]) {
+		System.out.println("PROGRAMME CLIENT");
+		
+		String serverHost = "127.0.0.1";
+		int serverPort = 1234;
+		Socket server;
+		
+		InputStream is;
+		DataInputStream dis;
+		int lengthIS;
+		
+		OutputStream os;
+		DataOutputStream dos;
+		
+		try {
+			server = new Socket(serverHost, serverPort);
+			System.out.println("Connected to " + server.getInetAddress());
+			
+			// Emission du nom de la machine
+			os = server.getOutputStream();
+			dos = new DataOutputStream(os);
+			String name = "Cocomo";
+			byte[] bOut = name.getBytes();
+			dos.writeInt(bOut.length);
+			dos.write(bOut);
+			System.out.println("Envoi du nom " + name);
+			
+			// Réception du message "Hello <client_name>"
+			is = server.getInputStream();
+			dis = new DataInputStream(is);
+			lengthIS = dis.readInt();
+			byte[] bIn = new byte[lengthIS];
+			
+			int nread = 0;
+			int num = 0;
+			
+			while(nread < lengthIS) {
+				num = dis.read(bIn, nread, lengthIS);
+				if (num == -1) {
+					return;
+				}
+				nread += num;
+			}
+			System.out.print("Server said: " + new String(bIn));
+			
+			server.close();
+			dos.close();
+			dis.close();
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/basicFileServer/Server.java
@@ -0,0 +1,57 @@
+package basicFileServer;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+	public static void main(String args[]) throws IOException {
+		System.out.println("PROGRAMME SERVEUR");
+		
+		while (true) {
+			// Le serveur est en attente de connexion
+			
+			ServerSocket server;
+			Socket client;
+			server = new ServerSocket(1234);
+			client = server.accept();
+			
+			// Un client est connecté
+			
+			System.out.println("Client " + client.getInetAddress() + " connected.");
+			
+			InputStream is = client.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			OutputStream os = client.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			
+			// Réception du nom du client
+			int length_in = dis.readInt();
+			byte[] b_in = new byte[length_in];
+			int nread = 0;
+			int num = 0;
+			while (nread < length_in) {
+				num = dis.read(b_in, nread, length_in-nread);
+				if (num == -1) {
+					return;
+				}
+				nread += num;
+			}
+			String client_name = new String(b_in);
+			System.out.println("Client said: " + client_name);
+			
+			// Emission du message "Hello <client_name>"
+			String message = "Hello " + client_name ;
+			byte[] b_out = message.getBytes();
+			dos.writeInt(b_out.length);
+			dos.write(b_out);
+			System.out.println("Reply: " + message);
+			
+			dis.close();
+			dos.close();
+			client.close();
+			server.close();
+		}
+	}
+
+}
