1c816bf1e5361282422998f221777093a2d7f283
Victor CUAU <victor.cuau@hotmail.fr>
2019-01-25 16:12:12 +0100

    basicMultiThreadedDesign : Création du nouveau package

---
+++ src/basicMultiThreadedDesign/Client.java
@@ -0,0 +1,101 @@
+package basicMultiThreadedDesign;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+	
+	static String folder = "/home/victor/Bureau/Applications réparties/ClientFiles/";
+	static String host = "127.0.0.1";
+	static int port = 1234;
+
+	public static void main(String args[]) {
+		System.out.println("PROGRAMME CLIENT");
+		
+		Socket server;
+		
+		InputStream is;
+		DataInputStream dis;
+		
+		OutputStream os;
+		DataOutputStream dos;
+		
+		try {
+			server = new Socket(host, port);
+			System.out.println("Connected to " + server.getInetAddress());
+			
+			// Saisie de la requête
+			Scanner sc = new Scanner(System.in);
+		  System.out.println("Request file? ");
+		  String filename = sc.nextLine();
+			
+			// Emission de la requête
+		  os = server.getOutputStream();
+			dos = new DataOutputStream(os);
+		  dos.writeInt(filename.length());
+			dos.write(filename.getBytes());
+			System.out.println("File request sent. Waiting for answer...");
+			
+			// Réception du code-réponse à la requête
+		  is = server.getInputStream();
+		  dis = new DataInputStream(is);
+			int length_in = dis.readInt();
+			byte[] b_in = new byte[length_in];
+			int nread = 0;
+			int num = 0;
+			while (nread < length_in) {
+				num = dis.read(b_in, nread, length_in-nread);
+				if (num < 0) {
+					return;
+				}
+				nread += num;
+			}
+			String reply = new String(b_in);
+
+			// Traitement du code de réponse
+			if (reply.equals("404")) {
+				System.out.println("ERROR 404: File not found");
+			}
+			else if (reply.equals("200")) {
+				System.out.println("200: File found");
+				System.out.println("Downloading...");
+
+				is = server.getInputStream();
+			  dis = new DataInputStream(is);
+				length_in = dis.readInt();
+				b_in = new byte[length_in];
+				nread = 0;
+				num = 0;
+				while (nread < length_in) {
+					num = dis.read(b_in, nread, length_in-nread);
+					if (num == -1) {
+						System.out.println("Download error.");
+						return;
+					}
+					nread += num;
+				}
+
+				FileWriter fstream = new FileWriter(folder + filename);
+		    BufferedWriter out = new BufferedWriter(fstream);
+		    out.write(new String(b_in));;
+		    out.close();
+		    fstream.close();
+		    System.out.println("Download succesfull!");
+			}
+			
+			server.close();
+			dos.close();
+			dis.close();
+			sc.close();
+			System.out.println("End of connection with " + server.getInetAddress());
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/basicMultiThreadedDesign/Server.java
@@ -0,0 +1,87 @@
+package basicMultiThreadedDesign;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	
+	static int port = 1234 ;
+	static String folder = "/home/victor/Bureau/Applications réparties/ServerFiles/";
+
+	public static void main(String args[]) throws IOException {
+		System.out.println("PROGRAMME SERVEUR");
+		
+		while (true) {
+			// Le serveur est en attente de connexion
+			
+			ServerSocket server;
+			Socket client;
+			server = new ServerSocket(port);
+			client = server.accept();
+			
+			// Un client est connecté
+			
+			System.out.println("Client " + client.getInetAddress() + " connected.");
+			
+			InputStream is = client.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			OutputStream os = client.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			
+			// Réception de la requête du client
+			int length_in = dis.readInt();
+			byte[] b_in = new byte[length_in];
+			int nread = 0;
+			int num = 0;
+			while (nread < length_in) {
+				num = dis.read(b_in, nread, length_in-nread);
+				if (num == -1) {
+					return;
+				}
+				nread += num;
+			}
+			String request = new String(b_in);
+			System.out.println("Download request: " + request);
+			
+			// Traitement de la requête
+			File fileRequest = new File(folder + request);
+			
+			if(fileRequest.isFile() && !fileRequest.isDirectory()) {
+				// Le fichier existe dans le répertoire
+				String ErrorCode = "200";
+				dos.writeInt(ErrorCode.length());
+				dos.write(ErrorCode.getBytes());
+				System.out.println("200: File found");
+				System.out.println("Uploading...");
+
+				byte[] bOut = new byte[(int) fileRequest.length()];
+				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileRequest));
+        int n = 0;
+        dos.writeInt((int) fileRequest.length());
+        while ((n = bis.read(bOut)) >= 0) {
+        	dos.write(bOut);
+        }
+        System.out.println("Upload succesfull!");
+				dos.flush();
+				bis.close();
+			}
+			
+			else {
+				// Le fichier n'existe pas dans le répertoire
+				String ErrorCode = "404";
+				os = client.getOutputStream();
+				dos = new DataOutputStream(os);
+				dos.writeInt(ErrorCode.length());
+				dos.write(ErrorCode.getBytes());
+				System.out.println("ERROR 404: File not found");
+			}
+			
+			dis.close();
+			dos.close();
+			client.close();
+			server.close();
+			System.out.println("Client " + client.getInetAddress() + " disconnected.");
+		}
+	}
+
+}
